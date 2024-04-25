cc8d59ca9d29a209893cbf2ce8ab42f5ca7b8fef
Parth <parthyadav141@gmail.com>
2024-04-25 14:06:38 +0530

    done

--- program1.js
+++ program1.js
@@ -1,5 +1,21 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+   
+
+
+   let  result = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (charIndexMap[s[i]] >= start) {
+            start = charIndexMap[s[i]] + 1;
+        }
+        charIndexMap[s[i]] = i;
+        result = Math.max(result, i - start + 1);
+    }
+
+   return result;
 }
 
 module.exports = { longestSubstring };
--- program2.js
+++ program2.js
@@ -1,7 +1,25 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  let n = nums.length;
+  let pre = new Array(n + 1);
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    pre[i] = false;
+  }
 
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0 && nums[i] <= n) {
+      pre[nums[i]] = true;
+    }
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!pre[i]) {
+      result = i;
+    }
+  }
+  return result;
 }
 
-module.exports = smallestMissingPositiveInteger;
+module.exports = { smallestMissingPositiveInteger };
